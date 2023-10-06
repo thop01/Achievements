ac8956ac69d443d4d9bfc5844381597aee5750f6
thop01 <p.thong@rocmondriaan.nl>
2023-10-07 00:23:42 +0200

    Vier cards gemaakt

--- app/src/main/java/com/example/achievements/MainActivity.kt
+++ app/src/main/java/com/example/achievements/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,9 +19,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,11 +40,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LessonView(
-                        title = stringResource(R.string.introduction),
-                        p1 = stringResource(R.string.node_intro_text),
-                        p2 = stringResource(R.string.node_intro_text2)
-                    )
+                    QuadrantView()
+                  //  TaskCompleteScreen()
+//                    LessonView(
+//                        title = stringResource(R.string.introduction),
+//                        p1 = stringResource(R.string.node_intro_text),
+//                        p2 = stringResource(R.string.node_intro_text2)
+//                    )
                   //  GreetingImage(msg = stringResource(R.string.congratulation_user), from = stringResource(R.string.from_pt))
                 }
             }
@@ -67,6 +73,67 @@ fun GreetingImage(msg: String, from: String, modifier:Modifier = Modifier){
         )
     }
 }
+
+@Composable
+fun QuadrantView() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            InfoCard(
+                title =" Operating System",
+                description = "Met de OS-Module kan je gegevens opvragen over jou besturingssysteem",
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            InfoCard(
+                title ="File System",
+                description = "Met de file system kan je CRUD-acties uitvoeren op bestanden.",
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            InfoCard(
+                title ="Console",
+                description = "Met de Console-module kan je informatie printen naar de terminal.",
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            InfoCard(
+                title ="Readline",
+                description = "Met een Readline kan je informatie van de gebruiker ontvangen.",
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+private fun InfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+        )
+    }
+}
+
 
 @Composable
 fun GreetingText(msg: String, from: String, modifier:Modifier = Modifier){
@@ -123,20 +190,50 @@ fun LessonView(title: String, p1: String, p2: String, modifier:Modifier = Modifi
 }
 
 @Composable
-fun AchievementPreview() {
-    AchievementsTheme {
-        GreetingImage(stringResource(R.string.congratulation_user), stringResource(R.string.from_pt))
+fun TaskCompleteScreen(modifier : Modifier = Modifier){
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+    ){
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = "Task Completed",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom= 8.dp)
+        )
+        Text(
+            text = "Nice work !",
+            fontWeight = FontWeight.Bold,
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun LessonPreview() {
+fun AchievementPreview() {
     AchievementsTheme {
-        LessonView(
-            title = stringResource(R.string.introduction),
-            p1 = stringResource(R.string.node_intro_text),
-            p2 = stringResource(R.string.node_intro_text2)
-        )
+
+//            GreetingImage(
+//                stringResource(R.string.congratulation_user),
+//                stringResource(R.string.from_pt)
+//            )
+
+//            LessonView(
+//                title = stringResource(R.string.introduction),
+//                p1 = stringResource(R.string.node_intro_text),
+//                p2 = stringResource(R.string.node_intro_text2)
+//            )
+
+        TaskCompleteScreen()
+        QuadrantView()
+
+
     }
 }
